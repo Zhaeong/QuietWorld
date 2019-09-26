bfe0f61a7b547f431cf8ec528099ed552cbf8867
Owen Zhang <owen_zzz@hotmail.com>
2019-09-26 16:34:08 -0400

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -411,6 +411,21 @@ void InitIntroUI(SDL_Renderer *renderer, SDL_Window *window, Texture *uiArray)
   uiArray[0] = startGameButton;
 }
 
+void InitInterLevelUI(SDL_Renderer *renderer, SDL_Window *window, Texture *uiArray)
+{
+  SDL_Texture *surveyBackgroundTex = GetSDLTexture(renderer, window, "res/dialogUI/survey.png");
+  Texture surveyBackground(surveyBackgroundTex, "res/dialogUI/survey.png");
+  surveyBackground.mX = 0;
+  surveyBackground.mY = 0;
+  uiArray[0] = surveyBackground;
+
+  SDL_Texture *startGameTex = GetSDLTexture(renderer, window, BTN_STARTGAME);
+  Texture startGameButton(startGameTex, BTN_STARTGAME);
+  startGameButton.mX = GAMEWIDTH / 2 - (startGameButton.mWidth / 2);
+  startGameButton.mY = GAMEHEIGHT * 2 / 3;
+  uiArray[1] = startGameButton;
+}
+
 void RenderUI(SDL_Renderer *renderer, Texture *uiArray, int size)
 {
   for (int i = 0; i < size; ++i)
